1e61758d697d004672f45e5c9e725726152b99a1
c0de <tkh1126@163.com>
2017-03-11 02:37:51 +0800

    完成双向队列

--- src/algs/queues/Deque.java
+++ src/algs/queues/Deque.java
@@ -1,15 +1,16 @@
 package algs.queues;
 
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class Deque<Item> {
+public class Deque<Item> implements Iterable<Item>{
 	private int size;
 	private Node first = null;
 	private Node last = null;
-	private class Node<Item> {
+	private class Node {
 		Item item;
-		Node last;
-		Node next;
+		Node lastNode;
+		Node nextNode;
 	}
 	public Deque() {
 		// construct an empty deque
@@ -25,46 +26,73 @@ public class Deque<Item> {
 	}
 	public void addFirst(Item item) {
 		// add the item to the front
-		if(item == null)
-			throw new NullPointerException();
-		Node<Item> oldfirst = this.first;
+		if(item == null) throw new NullPointerException();
+		Node oldfirst = this.first;
 		this.first = new Node();
 		this.first.item = item;
-		this.first.next = oldfirst;
+		this.first.nextNode = oldfirst;
 		if(this.size == 0)
 			this.last = this.first;
+		else
+			oldfirst.lastNode = this.first;
 		this.size++;
 	}
 	public void addLast(Item item) {
 		// add the item to the end
-		if(item == null)
-			throw new NullPointerException();
-		Node<Item> oldLast = this.last;
+		if(item == null) throw new NullPointerException();
+		Node oldLast = this.last;
 		this.last = new Node();
 		this.last.item = item;
-		this.last.last = oldLast;
+		this.last.lastNode = oldLast;
 		if(this.size == 0)
 			this.first = this.last;
+		else
+			oldLast.nextNode = this.last;
 		this.size++;
 	}
 	public Item removeFirst() {
 		// remove and return the item from the front
-		if(this.size == 0)
-			throw new NoSuchElementException();
-		Node<Item> oldFirst = this.first;
-		this.first = this.first.next;
+		if(this.size == 0) throw new NoSuchElementException();
+		Node oldFirst = this.first;
+		this.first = this.first.nextNode;
+		this.size--;
 		return oldFirst.item;
 	}
 	public Item removeLast() {
 		// remove and return the item from the end
-		Node<Item> oldLast = this.last;
-		this.last = this.last.last;
+		if(this.size == 0) throw new NoSuchElementException();
+		Node oldLast = this.last;
+		this.last = this.last.lastNode;
+		this.size--;
 		return oldLast.item;
 	}
-//	public Iterator<Item> iterator() {
-//		// return an iterator over items in order from front to end
-//	}
+	public Iterator<Item> iterator() {
+		// return an iterator over items in order from front to end
+		return new DequeIterator();
+	}
+	private class DequeIterator implements Iterator<Item>{
+
+		@Override
+		public boolean hasNext() {
+			return size != 0;
+		}
+
+		@Override
+		public Item next() {
+			return removeFirst();
+		}
+		
+	}
 	public static void main(String[] args) {
 		// unit testing (optional)
+		Deque<String> deque = new Deque<>();
+		deque.addFirst("s");
+		System.out.println(deque.removeLast());
+		System.out.println(deque.size());
+		deque.addFirst("a");
+		deque.addLast("b");
+		for(String a:deque) {
+			System.out.println(a);
+		}
 	}
 }
